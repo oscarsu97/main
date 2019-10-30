b6c435a65050a491a573e8734a74e2e8eeb6cb79
oscarsu97 <zhenhuanghuang97@gmail.com>
2019-10-30 22:20:17 +0800

    Fix checkStyle

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -33,17 +33,17 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Accommodation[] getSampleAccommodations() {
         return new Accommodation[]{
-                new Accommodation(new Name("Alex Yeoh"), new Address("Blk 30 Geylang Street 29, #06-40"),
+            new Accommodation(new Name("Alex Yeoh"), new Address("Blk 30 Geylang Street 29, #06-40"),
                         null, getTagSet("friends")),
-                new Accommodation(new Name("Bernice Yu"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+            new Accommodation(new Name("Bernice Yu"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                         null, getTagSet("colleagues", "friends")),
-                new Accommodation(new Name("Charlotte Oliveiro"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+            new Accommodation(new Name("Charlotte Oliveiro"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
                         null, getTagSet("neighbours")),
-                new Accommodation(new Name("David Li"), new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+            new Accommodation(new Name("David Li"), new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                         null, getTagSet("family")),
-                new Accommodation(new Name("Irfan Ibrahim"), new Address("Blk 47 Tampines Street 20, #17-35"),
+            new Accommodation(new Name("Irfan Ibrahim"), new Address("Blk 47 Tampines Street 20, #17-35"),
                         null, getTagSet("classmates")),
-                new Accommodation(new Name("Roy Balakrishnan"), new Address("Blk 45 Aljunied Street 85, #11-31"),
+            new Accommodation(new Name("Roy Balakrishnan"), new Address("Blk 45 Aljunied Street 85, #11-31"),
                         null, getTagSet("colleagues"))
         };
     }
@@ -58,22 +58,22 @@ public class SampleDataUtil {
 
     public static Activity[] getSampleActivities() {
         return new Activity[]{
-                new Activity(new Name("Alex Yeoh"), new Address("Blk 30 Geylang Street 29, #06-40"),
+            new Activity(new Name("Alex Yeoh"), new Address("Blk 30 Geylang Street 29, #06-40"),
                         null, getTagSet("friends"),
                         new Duration(30), new Priority(1)),
-                new Activity(new Name("Bernice Yu"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+            new Activity(new Name("Bernice Yu"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                         null, getTagSet("colleagues", "friends"),
                         new Duration(30), new Priority(2)),
-                new Activity(new Name("Charlotte Oliveiro"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+            new Activity(new Name("Charlotte Oliveiro"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
                         null, getTagSet("neighbours"),
                         new Duration(30), new Priority(3)),
-                new Activity(new Name("David Li"), new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+            new Activity(new Name("David Li"), new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                         null, getTagSet("family"),
                         new Duration(30), new Priority(4)),
-                new Activity(new Name("Irfan Ibrahim"), new Address("Blk 47 Tampines Street 20, #17-35"),
+            new Activity(new Name("Irfan Ibrahim"), new Address("Blk 47 Tampines Street 20, #17-35"),
                         null, getTagSet("classmates"),
                         new Duration(30), new Priority(5)),
-                new Activity(new Name("Roy Balakrishnan"), new Address("Blk 45 Aljunied Street 85, #11-31"),
+            new Activity(new Name("Roy Balakrishnan"), new Address("Blk 45 Aljunied Street 85, #11-31"),
                         null, getTagSet("colleagues"),
                         new Duration(30), new Priority(6))
         };
@@ -89,22 +89,22 @@ public class SampleDataUtil {
 
     public static Contact[] getSampleContacts() {
         return new Contact[]{
-                new Contact(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+            new Contact(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                         new Address("Blk 30 Geylang Street 29, #06-40"),
                         getTagSet("friends")),
-                new Contact(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+            new Contact(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                         new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                         getTagSet("colleagues", "friends")),
-                new Contact(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
+            new Contact(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                         new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
                         getTagSet("neighbours")),
-                new Contact(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
+            new Contact(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                         new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                         getTagSet("family")),
-                new Contact(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
+            new Contact(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                         new Address("Blk 47 Tampines Street 20, #17-35"),
                         getTagSet("classmates")),
-                new Contact(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
+            new Contact(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                         new Address("Blk 45 Aljunied Street 85, #11-31"),
                         getTagSet("colleagues"))
         };
@@ -126,7 +126,7 @@ public class SampleDataUtil {
         sampleActivities.add(new ActivityWithTime(a, LocalTime.of(10, 30), LocalTime.of(12, 30)));
 
         return new Day[]{
-                new Day(sampleActivities)
+            new Day(sampleActivities)
         };
 
     }
